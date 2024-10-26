9699c58a557aca1198dbdfecdb2b0dcec8137899
Thomas Harrison <theharrisoncrafter@gmail.com>
2024-10-25 23:53:48 -0400

    Only find fmt package if building examples

--- cmake/ProjectConfig.cmake
+++ cmake/ProjectConfig.cmake
@@ -3,7 +3,6 @@ set(RPLIDAR_VERSION @PROJECT_VERSION@)
 @PACKAGE_INIT@
 
 find_dependency(Threads REQUIRED)
-find_dependency(fmt 11.0 REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@Targets.cmake")
 
