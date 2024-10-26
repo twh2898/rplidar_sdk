586b1c168e9e84268dbe6d92e592a42470745f7d
Thomas Harrison <theharrisoncrafter@gmail.com>
2024-10-25 23:34:16 -0400

    Try targets

--- cmake/ProjectConfig.cmake
+++ cmake/ProjectConfig.cmake
@@ -2,6 +2,9 @@ set(RPLIDAR_VERSION @PROJECT_VERSION@)
 
 @PACKAGE_INIT@
 
+find_dependency(Threads)
 find_dependency(fmt)
+
+include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@Targets.cmake")
 
 check_required_components(@PROJECT_NAME@)
